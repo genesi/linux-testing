c2ac3bc7264122d71b372b32a4422e29a233c9a4
Richard Zhao <richard.zhao@linaro.org>
2012-01-13 11:09:56 +0800

    dma/imx-sdma: let sdma_run_channel call sdma_enable_channel
    Let all enable channel code call sdma_enable_channel.
    
    Signed-off-by: Richard Zhao <richard.zhao@linaro.org>
    Acked-by: Shawn Guo <shawn.guo@linaro.org>
    Acked-by: Sascha Hauer <s.hauer@pengutronix.de>
    Signed-off-by: Vinod Koul <vinod.koul@linux.intel.com>

--- drivers/dma/imx-sdma.c
+++ drivers/dma/imx-sdma.c
@@ -392,6 +392,11 @@ static int sdma_config_ownership(struct sdma_channel *sdmac,
 	return 0;
 }
 
+static void sdma_enable_channel(struct sdma_engine *sdma, int channel)
+{
+	__raw_writel(1 << channel, sdma->regs + SDMA_H_START);
+}
+
 /*
  * sdma_run_channel - run a channel and wait till it's done
  */
@@ -403,7 +408,7 @@ static int sdma_run_channel(struct sdma_channel *sdmac)
 
 	init_completion(&sdmac->done);
 
-	__raw_writel(1 << channel, sdma->regs + SDMA_H_START);
+	sdma_enable_channel(sdma, channel);
 
 	ret = wait_for_completion_timeout(&sdmac->done, HZ);
 
@@ -805,11 +810,6 @@ static int sdma_request_channel(struct sdma_channel *sdmac)
 out:
 
 	return ret;
-}
-
-static void sdma_enable_channel(struct sdma_engine *sdma, int channel)
-{
-	__raw_writel(1 << channel, sdma->regs + SDMA_H_START);
 }
 
 static dma_cookie_t sdma_assign_cookie(struct sdma_channel *sdmac)
